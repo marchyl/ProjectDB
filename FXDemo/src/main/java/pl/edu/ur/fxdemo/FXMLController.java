fea108b7b9ec24f39bb66d0f2de6416e17facccc
marcin <mchyla@stud.ur.edu.pl>
2019-03-18 08:06:01 +0100

    commit

--- FXDemo/src/main/java/pl/edu/ur/fxdemo/FXMLController.java
+++ FXDemo/src/main/java/pl/edu/ur/fxdemo/FXMLController.java
@@ -155,6 +155,7 @@ public class FXMLController implements Initializable {
                 
                 txtLastname.setText("");
                 txtName.setText("");
+                conn.close();
 
                 AlertUtil.showAlertInfo("Succes!", "New student added.", "");
             } else {
